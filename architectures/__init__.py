63c678bf58b408b94735b4fcff08b6c0de9464c2
Vitória <95037244+vitfrncs@users.noreply.github.com>
2026-07-23 08:26:29 -0300

    Add criar_convnextv2 to module exports

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -4,10 +4,11 @@ do resto do projeto."""
 from .mobilenet import criar_mobilenet
 from .efficientnet import criar_efficientnet_b0
 from .ghostnet import criar_ghostnet
+from .convnextv2 import criar_convnextv2
  
 __all__ = [
     "criar_mobilenet",
     "criar_efficientnet_b0",
     "criar_ghostnet",
+    "criar_convnextv2",
 ]
- 
